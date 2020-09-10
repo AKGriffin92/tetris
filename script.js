3aa88066e1f72a9760151ce86c84060c9cbc078b
AKGriffin92 <22640750+AKGriffin92@users.noreply.github.com>
2020-09-10 15:55:49 -0400

    bug fix

--- script.js
+++ script.js
@@ -70,8 +70,11 @@ function piece(tetromino, color){
 piece.prototype.draw = function() {
   for(r = 0; r < this.activeTetromino.length; r++){
     for(c = 0; c < this.activeTetromino,length; c++){
-      drawSquare(this.x + c,this.y + r, this.color);
-      console.log("test")
+      //only draw occupied squares
+      if(this.activeTetromino[r][c]){
+        drawSquare(this.x + c,this.y + r, this.color);
+        console.log("test")
+      };
     };
   };
 };
