7354d6e865d4bf278d1516645920f8bb7d42b651
AKGriffin92 <22640750+AKGriffin92@users.noreply.github.com>
2020-09-22 20:50:13 -0400

    change display font family

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ display.prototype.draw =  function(){
   ctx.strokeRect( this.x , this.y , this.width , this.height )
   
   if(this.text){    
-    ctx.font = "bold fantasy";
+    ctx.font = "bold verdana";
     ctx.textBaseline = "top";
     ctx.textAlign = "center";
     ctx.fillStyle = "black";
