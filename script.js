5a4e9688a12b7e1c75b5f3dfe1ca033f1e8b4feb
AKGriffin92 <22640750+AKGriffin92@users.noreply.github.com>
2020-09-22 20:45:04 -0400

    change display font family

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ display.prototype.draw =  function(){
   ctx.strokeRect( this.x , this.y , this.width , this.height )
   
   if(this.text){    
-    ctx.font = "bold system-ui";
+    ctx.font = "bold ui-monospace";
     ctx.textBaseline = "top";
     ctx.textAlign = "center";
     ctx.fillStyle = "black";
